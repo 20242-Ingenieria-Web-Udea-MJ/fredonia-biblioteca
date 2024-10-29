b33238fb85ecba2b42cc8e80aeff6f6d561a19cc
Esteban <estebancg12@hotmail.com>
2024-10-29 14:44:28 -0500

    Configurando next-auth, prisma y auth0 🧰

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,51 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/PrismaConfig";
+
+const options: NextAuthOptions = {
+  callbacks: {
+    async session({ session, user }) {
+      if (user) {
+        const userSession = await prisma.session.findFirst({
+          where: { userId: Number(user.id) },
+          include: { user: true },
+          orderBy: { expires: "desc" },
+        });
+
+        if (userSession && userSession.user) {
+          return {
+            ...session,
+            user: userSession.user,
+            token: userSession.sessionToken,
+          };
+        }
+      }
+      return session;
+    },
+  },
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID || "",
+      clientSecret: process.env.AUTH0_CLIENT_SECRET || "",
+      issuer: `https://${process.env.AUTH0_DOMAIN}`,
+      authorization: {
+        url: `https://${process.env.AUTH0_DOMAIN}/authorize`,
+        params: { response_type: "code", prompt: "login" },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "database",
+    maxAge: 3 * 24 * 60 * 60,
+  },
+};
+
+const authHandler = (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, options);
+
+export default authHandler;
+export { options };
