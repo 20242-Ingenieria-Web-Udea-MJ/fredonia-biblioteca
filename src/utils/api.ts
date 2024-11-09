b61e374f1ad31eedd36c616b994f846de6be9530
Esteban <estebancg12@hotmail.com>
2024-11-09 11:07:05 -0500

    Configuración para editar y eliminar usuario de Auth0 🧰

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -48,3 +48,58 @@ export async function createAuth0User(
   const auth0User = await response.json();
   return auth0User;
 }
+
+// Función para editar un usuario en Auth0
+export async function updateAuth0User(
+  userId: string,
+  updatedData: { email?: string; name?: string; password?: string },
+  accessToken: string,
+  tokenType: string
+) {
+  const response = await fetch(
+    `https://${AUTH0_DOMAIN}/api/v2/users/${userId}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${tokenType} ${accessToken}`,
+      },
+      body: JSON.stringify(updatedData),
+    }
+  );
+
+  if (!response.ok) {
+    console.error("Error editando el usuario en Auth0:", await response.text());
+    return null;
+  }
+
+  const updatedUser = await response.json();
+  return updatedUser;
+}
+
+// Función para eliminar un usuario en Auth0
+export async function deleteAuth0User(
+  userId: string,
+  accessToken: string,
+  tokenType: string
+) {
+  const response = await fetch(
+    `https://${AUTH0_DOMAIN}/api/v2/users/${userId}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `${tokenType} ${accessToken}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    console.error(
+      "Error eliminando el usuario en Auth0:",
+      await response.text()
+    );
+    throw new Error("Error eliminando el usuario en Auth0");
+  }
+
+  return true;
+}
