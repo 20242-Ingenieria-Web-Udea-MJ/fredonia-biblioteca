0fc3fe62429b123cdf6b8f155398895bfaaa33de
Esteban <estebancg12@hotmail.com>
2024-11-09 11:06:30 -0500

    Desarrollando funcionalidad de eliminar, editar y mostrar usuarios 🧰

--- src/pages/accounts/index.tsx
+++ src/pages/accounts/index.tsx
@@ -1,8 +1,55 @@
 import { UsersTable } from "@/components/molecules/UsersTable";
 import { Button } from "@/components/ui/button";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
+import { useSession, signIn } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Session } from "next-auth";
 
-export default function index() {
+interface ExtendedSession extends Session {
+  user: {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    role?: "ADMIN" | "USER";
+  };
+}
+
+export default function UsersPage() {
+  const { data: session, status } = useSession() as {
+    data: ExtendedSession | null;
+    status: "loading" | "authenticated" | "unauthenticated";
+  };
+  const router = useRouter();
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      signIn();
+    } else if (status === "authenticated" && session?.user?.role !== "ADMIN") {
+      setShowMessage(true);
+      const timer = setTimeout(() => {
+        router.replace("/");
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [status, session, router]);
+
+  if (status === "loading") {
+    return <LoadingSpinner />;
+  }
+
+  if (showMessage) {
+    return (
+      <div className="flex justify-center items-center w-full h-full p-12">
+        <p className="text-6xl font-bold text-destructive">
+          ❌ No tienes permiso para acceder a esta página. ❌
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="space-y-4">
       <Link href="/accounts/register" className="flex justify-end">
