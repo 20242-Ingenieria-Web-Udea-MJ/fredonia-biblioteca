36ea40cf3bba787d8912f8df59a6c730c9038b03
Daniel Brand <daniel.brand@guane.com.co>
2024-11-10 19:20:33 -0500

    Remove unused local font imports from the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "../lib/fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../lib/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
